0415b589fedf710c048b1a083b221867dcca7c35
Aitana Shough <aeshough@gmail.com>
2022-10-28 10:32:40 -0700

    WIP: update if branching method to detect 1s in array

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,12 @@ function beepBoop(number) {
   let array = [];
   for (let i = 0; i <= number; i++) {
     array.push(i);
-    if (array[i] === 1) {
-      let oneIndex = array.indexOf(1);
-      array.splice(oneIndex, 1, "Beep!")
+  }
+  const stringArray = array.map(String);
+  array.forEach(function () {
+    if ((stringArray[i][0] === 1) || (stringArray[i][1] ===1)) {
+      let index = array.indexOf(array[i]);
+      array.splice(index, 1, "Beep!")
     } else if (array[i] === 2) {
       let twoIndex = array.indexOf(2);
       array.splice(twoIndex, 1, "Boop!")
@@ -13,9 +16,38 @@ function beepBoop(number) {
       let threeIndex = array.indexOf(3);
       array.splice(threeIndex, 1, "Won't you be my neighbor?")
     }
+  });
+  return stringArray;
   }
-  return array;
+
+function oneToBeep(number) {
+  const array = beepBoop(number);
+  const stringArray = array.map(String);
+  for (i = 0; i <= stringArray.length; i++) {
+    if ((stringArray[i][0] === 1) || (stringArray[i][1] ===1)) {
+      let index = array.indexOf(array[i]);
+      array.splice(index, 1, "Beep!")
+    }
+  }
+  return stringArray;
 }
+
+// testing in dev console
+let stringArray = ['10','11','12','13'];
+for (let i = 0; i <= stringArray.length; i++) {
+  if (stringArray[i][i] === '1') {
+    let index = stringArray.indexOf(stringArray[i]);
+    stringArray.splice(index, 1, "Beep!");
+    console.log(stringArray);
+  } else if (stringArray[i] === '2') {
+    let twoIndex = stringArray.indexOf(2);
+    stringArray.splice(twoIndex, 1, "Boop!");
+  } else if (stringArray[i] === '3') {
+    let threeIndex = stringArray.indexOf(3);
+    stringArray.splice(threeIndex, 1, "Won't you be my neighbor?");
+  }
+};
+console.log(stringArray);
 
 // Hierarchy of Substitutions
 // neighbor (3) > boop (2) > beep (1)
