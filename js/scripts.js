8ca2b563b983b520e39b4962d98331b21c49e572
Aitana Shough <aeshough@gmail.com>
2022-11-06 13:46:28 -0800

    disable submit button unless results are cleared

--- js/scripts.js
+++ js/scripts.js
@@ -3,64 +3,18 @@
 function beepBoop(number) {
   let array = [];
   for (let i = 0; i <= number; i++) {
-    array.push(i);
+    let numString = String(i);
+    if (/3/.test(numString)) {
+      array.push("Won't you be my neighbor?");
+    } else if (/2/.test(numString)) {
+      array.push("Boop!");
+    } else if (/1/.test(numString)) {
+      array.push("Beep!");
+    } else {
+      array.push(i);
+    }
+    } return array;
   }
-  const stringArray = array.map(String);
-  for (let i = 0; i < stringArray.length; i++) {
-      if ((stringArray[i][0] === '3') || (stringArray[i][1] === '3') || (stringArray[i][2] === '3')) {
-        let index3 = stringArray.indexOf(stringArray[i]);
-        stringArray.splice(index3, 1, "Won't you be my neighbor?");
-      } else if ((stringArray[i][0] === '2') || (stringArray[i][1] === '2') || (stringArray[i][2] === '2')) {
-        let index2 = stringArray.indexOf(stringArray[i]);
-        stringArray.splice(index2, 1, "Boop!");
-      } else if ((stringArray[i][0] === '1') || (stringArray[i][1] === '1') || (stringArray[i][2] === '1')) {
-        let index1 = stringArray.indexOf(stringArray[i]);
-        stringArray.splice(index1, 1, "Beep!"); } 
-    } return stringArray;
-}
-
-function beepBoop(number) { 
-  let array = [];
-  for (let i = 0; i <= number; i++) {
-    array.push(i);
-  }
-  const string = array.join(" ");
-  for (let i = 0; i < string.length; i++) {
-    if 
-    string.replace(/3/g, "Won't you be my neighbor?");
-    string.replace(/2/g, "Boop!");
-    string.replace(/1/g, "Beep!");
-  } return string;
-}
-
-function beepBoop(number) {
-  let array = [];
-  for (let i = 0; i <= number; i++) {
-    array.push(i);
-    if (array[i].includes('3')) {
-      array.splice(array[i], 1, "Won't you be my neighbor?");
-    } else if (array[i].includes('2')) {
-      array.splice(array[i], 1, "Boop!");
-    } else if (array[i].includes('1')) {
-      array.splice(array[i], 1, "Beep!");
-    }  
-  } return array;
-}
-
-  const stringArray = array.map(String);
-  for (let i = 0; i < stringArray.length; i++) {
-      if (stringArray[i].includes('3')) {
-        let index3 = stringArray.indexOf(stringArray[i]);
-        stringArray.splice(index3, 1, "Won't you be my neighbor?");
-      } else if (stringArray[i].includes('2')) {
-        let index2 = stringArray.indexOf(stringArray[i]);
-        stringArray.splice(index2, 1, "Boop!");
-      } else if (stringArray[i].includes('1')) {
-        let index1 = stringArray.indexOf(stringArray[i]);
-        stringArray.splice(index1, 1, "Beep!"); } 
-    } return stringArray;
-}
-
 
 // UI logic
 
@@ -77,6 +31,8 @@ function getResult(event) {
   body.append(h3);
   pElement.append(result.join(", "));
   body.append(pElement);
+
+  document.getElementById("submit").disabled = true;
 }
 
 function removeResult (event) {
@@ -87,6 +43,7 @@ function removeResult (event) {
 
   h3.remove();
   pElement.remove();
+  document.getElementById("submit").disabled = false;
 }
 
 window.addEventListener("load", function() {
